397410abaff254b34bc79c7d30feaa3e0e3a1d9c
lionel509 <80290853+lionel509@users.noreply.github.com>
2024-06-08 12:51:02 -0400

    CPLearnModel.sql

--- generated/CPLearnModel.sql
+++ generated/CPLearnModel.sql
@@ -7,7 +7,7 @@ DROP TABLE IF EXISTS students;
 
 -- Create parent table 'students'
 CREATE TABLE students (
-    student_id INT NOT NULL,
+    student_id INT NOT NULL AUTO_INCREMENT,
     last_name CHAR(50) NOT NULL,
     first_name CHAR(50) NOT NULL,
     phone CHAR(20) NOT NULL,
@@ -17,7 +17,7 @@ CREATE TABLE students (
 
 -- Create parent table 'test_categories'
 CREATE TABLE test_categories (
-    test_category_id INT NOT NULL,
+    test_category_id INT NOT NULL AUTO_INCREMENT,
     test_category_name CHAR(255),  -- test category EX: SHSAT, SAT, ACT, etc.
     test_subject CHAR(255),        -- main subject like EX: Algebra, Geometry, etc.
     test_subject2 CHAR(255),       -- detailed subjects EX: Polynomial graphing, etc.
@@ -26,7 +26,7 @@ CREATE TABLE test_categories (
 
 -- Create 'tests' table which references 'test_categories'
 CREATE TABLE tests (
-    test_id INT NOT NULL,
+    test_id INT NOT NULL AUTO_INCREMENT,
     test_name CHAR(50) NOT NULL,
     test_category_id INT NOT NULL,
     test_file_path CHAR(255) NOT NULL,
@@ -36,22 +36,24 @@ CREATE TABLE tests (
 
 -- Create 'test_details' table which references 'tests'
 CREATE TABLE test_details (
+    detail_id INT NOT NULL AUTO_INCREMENT,
     test_id INT NOT NULL,
     question_id INT NOT NULL,
     question_type CHAR(10),  -- Multiple_choice or other types
     question_answer_choice CHAR(255),
     question_answer_details CHAR(255),
-    PRIMARY KEY (test_id, question_id),
+    PRIMARY KEY (detail_id),
     CONSTRAINT fk_test FOREIGN KEY (test_id) REFERENCES tests(test_id)
 );
 
 -- Create 'student_test_history' table which references 'students' and 'tests'
 CREATE TABLE student_test_history (
+    history_id INT NOT NULL AUTO_INCREMENT,
     test_id INT NOT NULL,
     test_date DATE NOT NULL,
     student_id INT NOT NULL,
     test_score DOUBLE PRECISION NOT NULL,  -- type mapped from: DOUBLE
-    PRIMARY KEY (test_id, test_date),
+    PRIMARY KEY (history_id),
     CONSTRAINT fk_student FOREIGN KEY (student_id) REFERENCES students(student_id),
     CONSTRAINT fk_test_history FOREIGN KEY (test_id) REFERENCES tests(test_id)
 );
