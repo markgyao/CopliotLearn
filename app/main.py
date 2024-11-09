5c828d4fbc3400a93547ef033309ee38025c64b4
jintian09 <jintianc1015@gmail.com>
2024-11-09 17:31:34 -0500

    initial commitment

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
-from app.routers import role, user, student
+from app.routers import role, user, student, auth
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+DATABASE_URL=os.getenv("DATABASE_URL")
+SECRET_KEY=os.getenv("SECRET_KEY")
 
 
 app = FastAPI()
@@ -8,3 +14,4 @@ app = FastAPI()
 app.include_router(role.router)
 app.include_router(user.router)
 app.include_router(student.router)
+app.include_router(auth.router)
