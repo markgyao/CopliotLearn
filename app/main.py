364331104fa471a920245d6da2b04dcbae9b0fa5
Mark Yao <markgyao@gmail.com>
2024-12-06 19:40:24 -0500

    change root_path to /app in main.py

--- app/main.py
+++ app/main.py
@@ -12,8 +12,8 @@ SECRET_KEY=os.getenv("SECRET_KEY")
 # Setup logging
 setup_logging()
 
-
-app = FastAPI()
+app = FastAPI(root_path="/api")
+#app = FastAPI()
 
 app.include_router(role.router)
 app.include_router(user.router)
